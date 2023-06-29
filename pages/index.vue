c251a493f321eb0361ccbd3cd8a950551dd6c865
Daniel Obot <danobot@hotmail.com>
2023-06-28 20:55:18 -0500

    feat: Added styles to website.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,86 @@
 <template>
     <div>
         <h2>Hello!</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate laboriosam dignissimos commodi, assumenda ipsam quam iste, sint perspiciatis eius ratione, temporibus voluptatibus consequuntur ut excepturi. Laborum ullam unde ipsum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptatum placeat quasi aliquam iure ullam molestiae, laudantium voluptatibus rem, quidem voluptates. Ex possimus omnis dolore labore expedita perspiciatis inventore iure?
+        </p>
     </div>
 </template>
 
